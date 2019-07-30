1f423ff16d92e0c64d40a1cddeaab2c7368fa1dc
utahkaA <utahka.akiba@gmail.com>
2019-07-31 01:13:02 +0900

    [fix] modified socket path

--- docker/rails/unicorn.rb
+++ docker/rails/unicorn.rb
@@ -1,7 +1,7 @@
 worker_processes 8
 
 pid "/var/run/unicorn.pid"
-listen "./tmp/sockets/unicorn.sock"
+listen "/var/tmp/unicorn.sock"
 
 stdout_path "./log/unicorn.stdout.log"
 stderr_path "./log/unicorn.stderr.log"
